0daa8c8cf97ee6d9840d4ca577f37a85fbc30e65
diegoalejop <diegoalejop3@gmail.com>
2024-11-10 17:48:09 -0500

    Modifica carpeta scripts

--- scripts/streamlit_main.py
+++ scripts/streamlit_main.py
@@ -8,7 +8,7 @@ import datetime
 import joblib
 from sklearn.preprocessing import StandardScaler
 
-df = pd.read_csv('data\BankChurners.csv')
+df = pd.read_csv('scripts\BankChurners.csv')
 df.drop(columns = ['Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_1', 
                       'Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_2' ], inplace = True)
 num_col = df.select_dtypes(include=['int', 'double']).columns
